279e82c049e754557f87d4d191130e839279e0b9
yudithchuctaya <yudithyesicachuctaya@gmail.com>
2017-08-10 22:20:37 +0200

    ultimo desafio terminado

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+var contenedor =document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul =document.getElementById("azul");
+var verde =document.getElementById("verde");
+var amarillo=document.getElementById("amarillo");
+
+ contenedor.style.position="relative";
+ contenedor.style.background="DarkGray";
+ contenedor.style.width="600px";
+ contenedor.style.padding="50px";
+ contenedor.style.left="10px"
+ azul.style.position="absolute";
+ azul.style.top ="250";
+ azul.style.right="0";
+ // azul.style.left="560";
+// azul.style.background="blue"
+ 
+
+
+
+
